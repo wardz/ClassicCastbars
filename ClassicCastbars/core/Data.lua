05e2e4f187e2febd778f55a63bb6e66f0513126b
wardz <thomaszrx@gmail.com>
2020-01-19 12:08:15 +0100

    remove War Stomp from pushback blacklist

--- ClassicCastbars/core/Data.lua
+++ ClassicCastbars/core/Data.lua
@@ -1562,7 +1562,6 @@ namespace.pushbackBlacklist = {
     [GetSpellInfo(19769)] = 1,      -- Thorium Grenade
     [GetSpellInfo(13278)] = 1,      -- Gnomish Death Ray
     [GetSpellInfo(20589)] = 1,      -- Escape Artist
-    [GetSpellInfo(20549)] = 1,      -- War Stomp
 }
 
 -- Casts that should be stopped on damage received
