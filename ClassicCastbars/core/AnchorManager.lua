c0ac7b07471c1e49e35a131fb311c7ae9ba333a2
wardz <10900212+wardz@users.noreply.github.com>
2022-08-03 22:23:24 +0200

    fixes for party frame anchoring

--- ClassicCastbars/core/AnchorManager.lua
+++ ClassicCastbars/core/AnchorManager.lua
@@ -96,10 +96,6 @@ local function GetUnitFrameForUnit(unitType, unitID, hasNumberIndex)
 
         local unitFrame = _G[name]
         if unitFrame then
-            if unitType == "party" or unitType == "arena" then
-                return unitFrame, name
-            end
-
             if unitFrame:IsVisible() then -- unit frame exists and also is in use (for party/arena we need to ignore this check)
                 return unitFrame, name
             end
@@ -125,6 +121,7 @@ local function GetPartyFrameForUnit(unitID)
     -- frames for custom addons
     for i = 1, 40 do
         local frame, frameName = GetUnitFrameForUnit("party", "party"..i, true)
+
         if frame and ((frame.unit and UnitGUID(frame.unit) == guid) or frame.lastGUID == guid) and frame:IsShown() then
             if useCompact then
                 if strfind(frameName, "PartyMemberFrame") == nil then
