acf36937d31414b3c0ae68f3574b498b4c34970f
wardz <10900212+wardz@users.noreply.github.com>
2023-08-29 00:55:06 +0200

    use npcID instead of npcName for npcCastTimeCache

--- ClassicCastbars/ClassicCastbars.lua
+++ ClassicCastbars/ClassicCastbars.lua
@@ -520,7 +520,8 @@ function addon:COMBAT_LOG_EVENT_UNFILTERED()
                     castTime = reducedTime
                 end
             else
-                local cachedTime = self.db.npcCastTimeCache[srcName .. spellName]
+                local _, _, _, _, _, srcNpcID = strsplit("-", srcGUID)
+                local cachedTime = self.db.npcCastTimeCache[srcNpcID .. spellName]
                 if cachedTime then
                     -- Use cached time stored from earlier sightings for NPCs.
                     -- This is because mobs have various cast times, e.g a lvl 20 mob casting Frostbolt might have
@@ -555,8 +556,9 @@ function addon:COMBAT_LOG_EVENT_UNFILTERED()
 
         -- Auto correct cast times for mobs (only non-channels)
         if not isSrcPlayer and not channelCast then
-            if not strfind(srcGUID, "Player-") then -- just incase player is mind controlled by an NPC
-                local cachedTime = self.db.npcCastTimeCache[srcName .. spellName]
+            local unitType, _, _, _, _, srcNpcID = strsplit("-", srcGUID)
+            if unitType ~= "Player" then -- just incase player is mind controlled by an NPC
+                local cachedTime = self.db.npcCastTimeCache[srcNpcID .. spellName]
                 if not cachedTime then
                     local cast = activeTimers[srcGUID]
                     if not cast or (cast and not cast.hasCastSlowModified and not next(cast.activeModifiers)) then
@@ -567,8 +569,8 @@ function addon:COMBAT_LOG_EVENT_UNFILTERED()
 
                             -- Whatever time was detected between SPELL_CAST_START and SPELL_CAST_SUCCESS will be the new cast time
                             local castTimeDiff = abs(castTime - origCastTime)
-                            if castTimeDiff <= 5000 and castTimeDiff > 250 then -- take lag into account
-                                self.db.npcCastTimeCache[srcName .. spellName] = castTime
+                            if castTimeDiff <= 5000 and castTimeDiff > 285 then -- take lag into account
+                                self.db.npcCastTimeCache[srcNpcID .. spellName] = castTime
                                 npcCastTimeCacheStart[srcGUID] = nil
                             end
                         end
